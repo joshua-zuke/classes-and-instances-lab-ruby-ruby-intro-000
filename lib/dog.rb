97e4504f9a87647d04bac4f8d0b28bfce6d82d3b
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-25 17:36:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,12 @@
 class Dog
+  def bark
+    puts "Woof Woof!"
+  end
 end
 
 fido = Dog.new
 snoopy = Dog.new
 lassie = Dog.new
+
+fido.bark
+bark
--- lib/person.rb
+++ lib/person.rb
@@ -3,3 +3,8 @@ end
 
 adele_goldberg = Person.new
 alan_kay = Person.new
+
+puts "#{alan_kay.object_id}"
+puts "#{adele_goldberg.object_id}"
+
+puts "#{alan_kay.methods}"
